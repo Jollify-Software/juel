437f73926a7a62bd6125b4e1428518329949d0b6
Luke <obrienluk89@gmail.com>
2022-03-09 09:15:29 +0000

    DragMove handler

--- src/_Utils/DragMoveListener.ts
+++ src/_Utils/DragMoveListener.ts
@@ -1,16 +1,14 @@
 export function DragMoveListener (event) {
-    var target = event.target
+    var target = $(event.target);
     // keep the dragged position in the data-x/data-y attributes
-    var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
-    var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
+    var x = (parseFloat(target.data('x')) || 0) + event.dx;
+    var y = (parseFloat(target.data('y')) || 0) + event.dy;
   
     // translate the element
-    target.style.transform =
-      target.style.transform =
-        'translate(' + x + 'px, ' + y + 'px)'
+    target.css('transform', `translate(${x}px, ${y}px)`);
   
     // update the posiion attributes
-    target.setAttribute('data-x', x)
-    target.setAttribute('data-y', y)
+    target.data('x', x);
+    target.data('y', y);
   }
   
