b9424a6e2e7019f45bd52d4092b672bba5c51510
Luke <obrienluk89@gmail.com>
2022-05-23 22:17:55 +0100

    Removed Radial Menu

--- src/RadialMenu/RadialMenuArgs.ts
+++
@@ -1,9 +0,0 @@
-import { MenuItem } from "../_Core/MenuItem";
-
-export interface RadialMenuArgs {
-    parent?: HTMLElement
-    size?:number
-    onClick?: (item: MenuItem) => void
-    menuItems?: MenuItem[]
-    closeOnClick?: boolean
-}
--- src/RadialMenu/RadialMenuDisplayMode.ts
+++
@@ -1,4 +0,0 @@
-export enum RadialMenuDisplayMode {
-    svg= "svg",
-    grid = "grid"
-}
