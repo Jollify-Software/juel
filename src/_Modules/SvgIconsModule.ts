947cc31da03966f3dcf4ee54826bf138f707e988
Luke <obrienluk89@gmail.com>
2022-06-01 21:14:32 +0100

    Bundle SVG icons

--- src/_Modules/SvgIconsModule.ts
+++ src/_Modules/SvgIconsModule.ts
@@ -1,13 +1,13 @@
-import error from "../../res/icons/error.svg";
-import info from "../../res/icons/info.svg";
-import question from "../../res/icons/question.svg";
-import success from "../../res/icons/success.svg";
-import warning from "../../res/icons/warning.svg";
+import icn_error from "bundle-text:../../res/icons/error.svg";
+import icn_info from "bundle-text:../../res/icons/info.svg";
+import icn_question from "bundle-text:../../res/icons/question.svg";
+import icn_success from "bundle-text:../../res/icons/success.svg";
+import icn_warning from "bundle-text:../../res/icons/warning.svg";
 
 export module SvgIcons {
-    export var error = error;
-    export var info = info;
-    export var question = question;
-    export var success = success;
-    export var warning = warning;
+    export var error = icn_error;
+    export var info = icn_info;
+    export var question = icn_question;
+    export var success = icn_success;
+    export var warning = icn_warning;
 }
