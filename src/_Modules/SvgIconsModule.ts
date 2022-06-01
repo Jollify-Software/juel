a3196ba66058aec8d508a906300ac0e8f2390e3c
Luke <obrienluk89@gmail.com>
2022-06-01 20:08:43 +0100

    Svg Icons

---
+++ src/_Modules/SvgIconsModule.ts
@@ -0,0 +1,13 @@
+import error from "../../res/icons/error.svg";
+import info from "../../res/icons/info.svg";
+import question from "../../res/icons/question.svg";
+import success from "../../res/icons/success.svg";
+import warning from "../../res/icons/warning.svg";
+
+export module SvgIcons {
+    export var error = error;
+    export var info = info;
+    export var question = question;
+    export var success = success;
+    export var warning = warning;
+}
