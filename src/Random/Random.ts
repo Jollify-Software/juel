5680413d16f99148f8a192af61f16bf36a21de50
Luke <obrienluk89@gmail.com>
2022-06-28 11:44:34 +0100

    Random component

--- src/Random/Random.ts
+++ src/Random/Random.ts
@@ -1,30 +1,33 @@
-import { LitElement } from "lit";
+import { CSSResultGroup, html, LitElement, PropertyValueMap, unsafeCSS } from "lit";
 import { property, customElement } from "lit/decorators";
+import { choose } from "lit/directives/choose";
+import { createRef, ref, Ref } from "lit/directives/ref";
+import styles from "bundle-text:./Random.less";
 
 @customElement("juel-random")
 export class JuelRandom extends LitElement {
-    @property() selector: string;
-    @property() attr: string;
+    
+    static styles = unsafeCSS(styles);
+
+    @property() type: string;
     @property() values: string;
     @property() trigger: string;
 
+    @property() value: string;
+
+    elementRef: Ref<HTMLElement> = createRef();
+
     constructor() {
         super();
-        this.attr = "src";
+        this.type = "img"
         this.trigger = "load"
     }
 
-    createRenderRoot() {
-        return this;
-    }
-
     firstUpdated() {
-        setTimeout(() => {
-            if (this.selector && this.values) {
-                let el = document.querySelector(this.selector);
+            if (this.values) {
                 switch (this.trigger) {
                     case "load":
-                        this.setValue(el as HTMLElement);
+                        this.setValue();
                         break;
                     default:
                         let interval: number;
@@ -33,26 +36,40 @@ export class JuelRandom extends LitElement {
                         }
                         if (interval) {
                             setInterval(() => {
-                                this.setValue(el as HTMLElement);
+                                this.setValue();
                             }, interval);
                         }
                         break;
                 }
             }
-        });
     }
 
-    setValue(el: HTMLElement) {
-        if (this.attr in el) {
+    setValue() {
             let splitty = this.values.split(' ');
-            el[this.attr] = splitty[Math.floor(Math.random() * splitty.length)];
-            let p = el.parentElement;
+            this.value = splitty[Math.floor(Math.random() * splitty.length)];
+            /*let p = el.parentElement;
             if (p.nodeName == "AUDIO" || p.nodeName == "VIDEO") {
                 (<HTMLAudioElement>p).load();
                 if (p.hasAttribute("autoplay")) {
                     (<HTMLAudioElement>p).play();
                 }
+            }*/
+    }
+
+    protected updated(): void {
+        if (this.elementRef.value) {
+            let el = this.elementRef.value;
+            (<HTMLAudioElement>el).load();
+            if (el.hasAttribute("autoplay")) {
+                (<HTMLAudioElement>el).play();
             }
         }
+    }
+
+    render() {
+        return html`${choose(this.type, [
+            [ 'img', () => html`<img src="${this.value}" />` ],
+            [ 'video', () => html`<video ${ref(this.elementRef)} autoplay loop muted><source src="${this.value}"></video>` ]
+        ])}`
     }
 }
