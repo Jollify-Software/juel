9dc70a983ec24c4aa1a43731ae5c89b12ed750b6
Luke <obrienluk89@gmail.com>
2022-07-09 09:33:53 +0100

    InputBase - InputRef

--- src/_Templates/ButtonTemplate.ts
+++ src/_Templates/ButtonTemplate.ts
@@ -1,6 +1,7 @@
 import { html } from "lit";
+import { ref } from "lit/directives/ref";
 import { JuelButton } from "../Button/Button";
 
 export function ButtonTemplate(el: JuelButton) {
-    return html`<button type="${el.submit ? "submit" : "button"}" part="button" class="btn" part="button" @click="${el.onClick}"><slot name="content">${el.label}</slot></button>`;
+    return html`<button ${ref(el.input)} type="${el.submit ? "submit" : "button"}" part="button" class="btn" part="button" @click="${el.onClick}"><slot name="content">${el.label}</slot></button>`;
 }
--- src/_Templates/MemoTemplate.ts
+++ src/_Templates/MemoTemplate.ts
@@ -1,7 +1,8 @@
 import { html } from "lit";
 import { ifDefined } from "lit/directives/if-defined";
+import { ref } from "lit/directives/ref";
 import { JuelMemo } from "../Memo/Memo";
 
 export function MemoTemplate(el: JuelMemo) {
-    return html`<label part="label" for="text"><slot name="content">${el.label}</slot></label><textarea .value=${ifDefined(el.value)}></textarea>`;
+    return html`<label part="label" for="text"><slot name="content">${el.label}</slot></label><textarea ${ref(el.input)} .value=${ifDefined(el.value)}></textarea>`;
 }
