4ca21b3858e467169d72604b8becf796dcc87997
Luke <obrienluk89@gmail.com>
2022-07-29 22:31:58 +0100

    Remove copy data attributes

--- src/_Templates/ButtonTemplate.ts
+++ src/_Templates/ButtonTemplate.ts
@@ -4,5 +4,5 @@ import { JuelButton } from "../Button/Button";
 import { attr } from "../_Directives/AttrDirective";
 
 export function ButtonTemplate(el: JuelButton) {
-    return html`<button ${ref(el.input)} ${attr("data", el)} type="${el.submit ? "submit" : "button"}" part="button" class="btn" part="button" @click="${el.onClick}"><slot name="content">${el.label}</slot></button>`;
+    return html`<button ${ref(el.input)} type="${el.submit ? "submit" : "button"}" part="button" class="btn" part="button" @click="${el.onClick}"><slot name="content">${el.label}</slot></button>`;
 }
