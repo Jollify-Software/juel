5d86aa31f035f14ee704d3495e1777ef09bbdafe
Luke <obrienluk89@gmail.com>
2024-12-20 12:29:37 +0000

    Nav - Sticky

--- src/Navigation/Nav/Nav.ts
+++ src/Navigation/Nav/Nav.ts
@@ -1,17 +1,41 @@
-import { CSSResultGroup, html, LitElement, unsafeCSS } from "lit";
+import { CSSResultGroup, html, LitElement, PropertyValues, unsafeCSS } from "lit";
 import { customElement, property } from "lit/decorators";
 import Styles from 'bundle-text:./Nav.less';
 import { classMap } from "lit/directives/class-map";
+import { createRef, ref, Ref } from "lit/directives/ref";
 
 @customElement('juel-nav')
 export class JuelNav extends LitElement {
 
   static styles?: CSSResultGroup = unsafeCSS(Styles);
 
+  nav: Ref<HTMLInputElement> = createRef();
+
   // Property to toggle the hamburger menu
   @property({ type: Boolean })
   open: boolean = false;
+  @property({ type: Boolean })
+  sticky: boolean = true;
 
+  protected firstUpdated(_changedProperties: PropertyValues): void {
+    if (this.sticky) {
+      this.stickNavbar();
+    }
+  }
+  stickNavbar() {
+    document.addEventListener('DOMContentLoaded', () => {
+      const navbar = this.nav.value as HTMLElement; // Get the navbar element
+      const navbarOffset = navbar.offsetTop; // Get the initial position of the navbar
+      
+      window.addEventListener('scroll', function () {
+        if (window.scrollY > navbarOffset) {
+          navbar.classList.add('fixed');
+        } else {
+          navbar.classList.remove('fixed');
+        }
+      });
+    });
+  }
 
   // Toggle the menu when the hamburger is clicked
   private toggleMenu() {
@@ -23,7 +47,7 @@ export class JuelNav extends LitElement {
     let menuClass = { open: this.open };
 
     return html`
-      <nav>
+      <nav ${ref(this.nav)}>
         <div class="logo"><slot name="logo"></slot></div>
         <div class="menu-toggle ${classMap(menuClass)}" @click="${this.toggleMenu}">
           <div></div>
